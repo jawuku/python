09aec2a1b8567a2613e35738b18252b81adc9bae
Jason Awuku <41336414+jawuku@users.noreply.github.com>
2021-08-26 23:40:27 +0100

    Update realpi.py

--- realpi.py
+++ realpi.py
@@ -7,7 +7,7 @@ prec = input ('Enter bits of precision : ')
 # catch error if floating point entered
 # convert to integer if floating point entered
 try:
-    prec = int (float(prec))
+    prec = int(float(prec))
 
 except ValueError:
     prec = 0
@@ -17,26 +17,24 @@ except ValueError:
 
 if prec <=1:
 
-    print ('\nEnter an integer value greater than 1. Setting to 256-bit precision')
+    print('\nEnter an integer value greater than 1. Setting to 256-bit precision')
     gmpy2.get_context().precision = 256
 
 else:
-    print ('\nSetting to {}-bit precision'.format(prec))
+    print('\nSetting to {}-bit precision'.format(prec))
     gmpy2.get_context().precision = prec
 
 # Perform calculation
 # calc variable stores intermediate values in calculation
 calc = mpfr('5')
-calc = gmpy2.sqrt (calc)
-calc = gmpy2.add (1, calc)
+calc = gmpy2.sqrt(calc)
+calc = gmpy2.add(1, calc)
 
-phi = gmpy2.div (calc, 2)
+phi = gmpy2.div(calc, 2)
 
-rootphi = gmpy2.sqrt (phi)
+rootphi = gmpy2.sqrt(phi)
 
-realpi = gmpy2.div (4, rootphi)
+realpi = gmpy2.div(4, rootphi)
 
-print ('Real value of pi = ')
-print (realpi)
-
-
+print('Real value of pi = ')
+print(realpi)
